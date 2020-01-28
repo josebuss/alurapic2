d04ecc76cdeb2729a4d7fd29f7fdcbd60ebcefad
José Buss <josecarlosbuss@gmail.com>
2020-01-27 22:09:02 -0300

    Inicio
    Projeto inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
+import { AppRountingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule,
+    AppRountingModule,
+    ErrorsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

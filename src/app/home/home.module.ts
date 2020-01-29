c55e09174d28ac516ee777e0da033cb2f977af1c
José Buss <josecarlosbuss@gmail.com>
2020-01-28 22:16:31 -0300

    aula 2
    aula 2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SingInComponent } from './sing-in/sing-in.component';
+import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    VmessageModule
   ],
   declarations: [SingInComponent]
 })
--- src/app/home/sing-in/sing-in.component.ts
+++ src/app/home/sing-in/sing-in.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
   selector: 'ap-sing-in',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingInComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private platformService: PlatformDetectorService,
+    private router: Router) {
+  }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
+
+  login() {
+    const userName = this.loginForm.get("userName").value;
+    this.authService.autenticate(
+      userName,
+      this.loginForm.get("password").value).subscribe(
+        () => { this.router.navigate(["user", userName]) },
+        erro => {
+          console.log(erro);
+          this.loginForm.reset();
+          this.platformService.isPlatformBrowser() && this.userNameInput.nativeElement.focus();
+          //this.platformService.isPlatformBrowser() ? this.userNameInput.nativeElement.focus() : null;
+        }
+      );
+  };
 
 }
